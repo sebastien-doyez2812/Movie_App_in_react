6eee1d08545556254f3b4a052214dab3204b9cce
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-06-23 19:18:49 -0400

    Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard  from './components/movies_card'
+import Home from './pages/home'
+
 
 function App() {
-  const cas = 2;
-  const my_movie = {
-    title: "Terminator",
-    release_date: "2024"
-  }
-  const defaut =  {
-    title: "Steven Seagal",
-    release_date: "1986"
-  }
    return ( 
     <>
-    {cas == 1? 
-    (<MovieCard movie={my_movie}/>):
-    (<MovieCard movie={defaut}/>)
-    }
+      <Home></Home>
     </>
   );   
 }
